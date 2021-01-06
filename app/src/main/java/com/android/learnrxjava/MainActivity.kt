cb1c90fa41792fd03f6412b1c74b46e11e1a9093
sachinchugh <techdomain0502@gmail.com>
2021-01-06 07:30:08 +0530

    set result from observable stream on textview

--- app/src/main/java/com/android/learnrxjava/MainActivity.kt
+++ app/src/main/java/com/android/learnrxjava/MainActivity.kt
@@ -1,46 +1,50 @@
 package com.android.learnrxjava
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import com.android.learnrxjava.utils.Logger
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
-import io.reactivex.rxjava3.annotations.SchedulerSupport.IO
 import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.core.Observer
-import io.reactivex.rxjava3.core.Scheduler
 import io.reactivex.rxjava3.disposables.Disposable
 import io.reactivex.rxjava3.schedulers.Schedulers
 
 class MainActivity : AppCompatActivity() {
 
-        val tag = MainActivity::class.java.simpleName
+    val tag = MainActivity::class.java.simpleName + "_log"
 
     //using Observable<T>.just to get observable from string data stream
-    private lateinit var greetObservable:Observable<String>
+    private lateinit var greetObservable: Observable<String>
 
-    private lateinit var greetObserver:Observer<String>
+    private lateinit var greetObserver: Observer<String>
+
+    private lateinit var textView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        greetObservable  = Observable.just("hello rxjava")
+        textView = findViewById(R.id.textview)
 
-        greetObserver = object:Observer<String>{
+        greetObservable = Observable.just("hello from rxjava")
+
+        greetObserver = object : Observer<String> {
             override fun onSubscribe(d: Disposable?) {
-                Logger.logd(tag,"onsubscribe ")
+                Logger.logd(tag, "onsubscribe ${Thread.currentThread().name}")
             }
 
             override fun onNext(t: String?) {
-                Logger.logd(tag,"onnext ${t} ")
+                Logger.logd(tag, "onnext ${t} ${Thread.currentThread().name}")
+                textView.text = t
             }
 
             override fun onError(e: Throwable?) {
-                Logger.logd(tag," onerror")
+                Logger.logd(tag, " onerror ${Thread.currentThread().name}")
             }
 
             override fun onComplete() {
-                Logger.logd(tag,"oncomplete")
+                Logger.logd(tag, "oncomplete ${Thread.currentThread().name}")
             }
 
         }
