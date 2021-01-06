5a811da1805ab76a2b2526319795208fa3d70fb2
sachinchugh <techdomain0502@gmail.com>
2021-01-06 07:47:46 +0530

    explore disposable and compositedisposable

--- app/src/main/java/com/android/learnrxjava/MainActivity.kt
+++ app/src/main/java/com/android/learnrxjava/MainActivity.kt
@@ -2,11 +2,13 @@ package com.android.learnrxjava
 
 import android.os.Bundle
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.android.learnrxjava.utils.Logger
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.core.Observer
+import io.reactivex.rxjava3.disposables.CompositeDisposable
 import io.reactivex.rxjava3.disposables.Disposable
 import io.reactivex.rxjava3.schedulers.Schedulers
 
@@ -19,7 +21,13 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var greetObserver: Observer<String>
 
+    private lateinit var greetObserver2: Observer<String>
+
     private lateinit var textView: TextView
+
+    private var disposable: Disposable? = null
+
+    private lateinit var compositeDisposable: CompositeDisposable
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,22 +37,26 @@ class MainActivity : AppCompatActivity() {
 
         greetObservable = Observable.just("hello from rxjava")
 
+        compositeDisposable = CompositeDisposable()
+
         greetObserver = object : Observer<String> {
             override fun onSubscribe(d: Disposable?) {
-                Logger.logd(tag, "onsubscribe ${Thread.currentThread().name}")
+                Logger.logd(tag, "greetobservable1 onsubscribe ${Thread.currentThread().name}")
+                disposable = d
+                compositeDisposable.add(d)
             }
 
             override fun onNext(t: String?) {
-                Logger.logd(tag, "onnext ${t} ${Thread.currentThread().name}")
+                Logger.logd(tag, "greetobservable1 onnext ${t} ${Thread.currentThread().name}")
                 textView.text = t
             }
 
             override fun onError(e: Throwable?) {
-                Logger.logd(tag, " onerror ${Thread.currentThread().name}")
+                Logger.logd(tag, "greetobservable1  onerror ${Thread.currentThread().name}")
             }
 
             override fun onComplete() {
-                Logger.logd(tag, "oncomplete ${Thread.currentThread().name}")
+                Logger.logd(tag, "greetobservable1 oncomplete ${Thread.currentThread().name}")
             }
 
         }
@@ -53,5 +65,50 @@ class MainActivity : AppCompatActivity() {
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe(greetObserver)
 
+        //add one more observer
+        greetObserver2 = object : Observer<String> {
+            override fun onSubscribe(d: Disposable?) {
+                Logger.logd(tag, "greetobservable2 onsubscribe ${Thread.currentThread().name}")
+                disposable = d
+                compositeDisposable.add(d)
+            }
+
+            override fun onNext(t: String?) {
+                Logger.logd(tag, "greetobservable2 onnext ${t} ${Thread.currentThread().name}")
+                textView.text = t
+            }
+
+            override fun onError(e: Throwable?) {
+                Logger.logd(tag, "greetobservable2  onerror ${Thread.currentThread().name}")
+            }
+
+            override fun onComplete() {
+                Logger.logd(tag, "greetobservable2 oncomplete ${Thread.currentThread().name}")
+            }
+
+        }
+     // lets subscribe the observer 2
+        greetObservable
+            .subscribe(greetObserver2)
+
+        Toast.makeText(this,"current observers registered = ${compositeDisposable.size()}",Toast.LENGTH_SHORT).show()
+    }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        //lets clean up the subscription made above by observer1
+        disposable?.run {
+            dispose()
+        }
+
+        Logger.logd(tag,"composite disposable size before disposition= ${compositeDisposable.size()}")
+
+        //collective disposition for all observers
+        compositeDisposable.run {
+            dispose()
+        }
+        Toast.makeText(this,"current observers registered = ${compositeDisposable.size()}",Toast.LENGTH_SHORT).show()
+        Logger.logd(tag,"composite disposable size after disposition= ${compositeDisposable.size()}")
     }
 }
