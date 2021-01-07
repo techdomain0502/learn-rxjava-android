c6a4831f477a4b2fa5c339bddd8e46dd9c3aa407
sachinchugh <techdomain0502@gmail.com>
2021-01-07 14:39:46 +0530

    async subject demo

--- app/src/main/java/com/android/learnrxjava/MainActivity.kt
+++ app/src/main/java/com/android/learnrxjava/MainActivity.kt
@@ -14,42 +14,74 @@ import io.reactivex.rxjava3.disposables.Disposable
 import io.reactivex.rxjava3.functions.Function
 import io.reactivex.rxjava3.observers.DisposableObserver
 import io.reactivex.rxjava3.schedulers.Schedulers
+import io.reactivex.rxjava3.subjects.AsyncSubject
 
 class MainActivity : AppCompatActivity() {
 
     val tag = MainActivity::class.java.simpleName + "_log"
+
+    fun getObserver_1():Observer<String>{
+         return object:Observer<String>{
+             override fun onSubscribe(d: Disposable?) {
+                 Logger.logd(tag,"observer 1 onsubscribe")
+             }
+
+             override fun onNext(t: String?) {
+                 Logger.logd(tag,"observer 1 onnext ${t}")
+             }
+
+             override fun onError(e: Throwable?) {
+                 Logger.logd(tag,"observer 1 onerror")
+             }
+
+             override fun onComplete() {
+                 Logger.logd(tag,"observer 1 oncomplete")
+             }
+
+         }
+    }
+
+    fun getObserver_2():Observer<String>{
+      return object:Observer<String>{
+          override fun onSubscribe(d: Disposable?) {
+              Logger.logd(tag,"observer 2 onsubscribe")
+          }
+
+          override fun onNext(t: String?) {
+              Logger.logd(tag,"observer 2 onnext ${t}")
+          }
+
+          override fun onError(e: Throwable?) {
+              Logger.logd(tag,"observer 2 onerror")
+          }
+
+          override fun onComplete() {
+              Logger.logd(tag,"observer 2 oncomplete")
+          }
+
+      }
+    }
+
+    fun getObservable():Observable<String>{
+        var arr = arrayOf("a","b","c")
+        return Observable.fromArray(*arr)
+    }
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val observable:Observable<Int> = Observable.just(1,2,3,4,5)
-        val observer:Observer<Int> = object:Observer<Int>{
-            override fun onSubscribe(d: Disposable?) {
-            Logger.logd(tag,"onsubscribe called")
-            }
+        val observer1 = getObserver_1()
+        val observer2 = getObserver_2()
 
-            override fun onNext(t: Int) {
-                    Logger.logd(tag," $t")
-                }
-
-            override fun onError(e: Throwable?) {
-            }
-
-            override fun onComplete() {
-            }
-        }
-
-        observable.subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
-            .skipLast(2)
-            .subscribe(observer)
+        val asyncSubject:AsyncSubject<String> = AsyncSubject.create()
+        getObservable().subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread())
+            .subscribe(asyncSubject)
+        asyncSubject.subscribe(observer1)
+        asyncSubject.subscribe(observer2)
 
         }
-
-
-
 
     }
 
