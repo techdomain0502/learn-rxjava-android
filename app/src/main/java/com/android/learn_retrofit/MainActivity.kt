9e0a0fbc8b7ed74d01f9b82dd096172bf1c943d2
sachinchugh <techdomain0502@gmail.com>
2021-01-10 07:45:51 +0530

    back to old schools - lets simply use flatmap to just map each item into observable which  is then observed. If we had used map instead of flatmap, it would have emitted all items as observable in one go, which we dont need. instead we need to get each item one by one and which can be observed. So in this case flatmap is the optimal choice

--- app/src/main/java/com/android/learn_retrofit/MainActivity.kt
+++ app/src/main/java/com/android/learn_retrofit/MainActivity.kt
@@ -6,8 +6,10 @@ import android.util.Log
 import android.view.View
 import android.widget.ProgressBar
 import com.android.learn_retrofit.models.Country
+import com.android.learn_retrofit.models.CountryItem
 import com.android.learn_retrofit.network.RetrofitInstance
 import io.reactivex.FlowableSubscriber
+import io.reactivex.Observable
 import io.reactivex.Observer
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
@@ -23,7 +25,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
-        val asyncSubject:AsyncSubject<Country> = AsyncSubject.create()
+
         val retrofitInstance = RetrofitInstance()
         val progressBar:ProgressBar = findViewById(R.id.progressbar)
         retrofitInstance.getCountryServiceApi()
@@ -31,29 +33,33 @@ class MainActivity : AppCompatActivity() {
              getAllCountries()
                  .subscribeOn(Schedulers.io())
                  .observeOn(AndroidSchedulers.mainThread())
-                 .subscribe(asyncSubject)
-            }
+            }?.flatMap {
+               Log.d(TAG,"flat map called")
+                val countryArray = it.toTypedArray()
+                 Observable.fromArray(*countryArray)
+                }
+                ?.subscribe(object:Observer<CountryItem>{
+                    override fun onSubscribe(d: Disposable) {
+                        Log.d(TAG,"onsubscribe called")
+                        progressBar.visibility = View.VISIBLE
+                    }
 
-        asyncSubject.subscribe(object:Observer<Country>{
-            override fun onSubscribe(d: Disposable) {
-                Log.d(TAG,"onsubscribe")
-                progressBar.visibility = View.VISIBLE
-            }
+                    override fun onNext(t: CountryItem) {
+                        Log.d(TAG,"onnext called ${t}")
+                    }
 
-            override fun onNext(t: Country) {
-                Log.d(TAG,"${t.size}")
-            }
+                    override fun onError(e: Throwable) {
+                        TODO("Not yet implemented")
+                    }
 
-            override fun onError(e: Throwable) {
-                TODO("Not yet implemented")
-            }
+                    override fun onComplete() {
+                        Log.d(TAG,"oncomplete")
+                        progressBar.visibility = View.INVISIBLE
+                    }
 
-            override fun onComplete() {
-                Log.d(TAG,"oncomplete")
-                progressBar.visibility = View.INVISIBLE
-            }
+                })
 
-        })
+
 
 
     }
